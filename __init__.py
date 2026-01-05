db6471efa456b18de3a465066a2da699c89eeeae
Anand <2024aa05086@wilp.bits-pilani.ac.in>
2026-01-05 23:15:19 +0530

    health-check-model 2

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-"""Heart Disease MLOps project package."""
+"""
+heart_disease_mlops package.
+
+This file makes 'heart_disease_mlops' a Python package so imports like:
+  python -m heart_disease_mlops.src.train
+work in CI and locally.
+"""
+__all__ = []
